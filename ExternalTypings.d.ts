65e4e57582ab8ba6fb575e768483b62f968f54e7
Mehrdad Reshadi <mreshadi@instartlogic.com>
2016-05-20 00:43:16 -0700

    Update typings info for testTask

--- ExternalTypings.d.ts
+++ ExternalTypings.d.ts
@@ -1,3 +1,11 @@
 /// <reference path="typings/main/ambient/jake/index.d.ts" />
 /// <reference path="typings/main/ambient/shelljs/index.d.ts" />
 /// <reference path="typings/main.d.ts" />
+
+/**
+ * Creates a Jake TestTask.
+ * @name name The name of the Task
+ * @param prereqs Prerequisites to be run before this task
+ * @param definitions The function to update/add to TestTask members
+ */
+declare function testTask(name:string, prereqs?:string[], definitions?:()=>void): jake.TestTask;
