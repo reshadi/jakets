f837890d8012fb6d915b94d1a0aa6d1a4ed0e531
Mehrdad Reshadi <mreshadi@instartlogic.com>
2016-11-29 17:56:19 -0800

    added the missing exports for backward compatibility

--- Jakefile.ts
+++ Jakefile.ts
@@ -8,15 +8,19 @@ export let LogTask = Jake.LogTask;
 import * as Util from "./Util";
 export let MakeRelativeToWorkingDir = Util.MakeRelativeToWorkingDir;
 export let MakeRelative = Util.MakeRelativeToWorkingDir;
+export let LocalDir = Util.LocalDir;
+export let BuildDir = Util.BuildDir;
 
 import * as Bower from "./Bower";
 export let bower = Bower.Exec;
 
 import * as Tsc from "./Tsc";
 export let tsc = Tsc.Exec;
+export let TscTask = Tsc.TscTask;
 
 import * as Browserify from "./Browserify";
 export let browserify = Browserify.Exec;
+export let BrowserifyTask = Browserify.BrowserifyTask;
 
 import * as Closure from "./Closure";
 export let closure = Closure.Exec;
--- bootstrap/Bower.js
+++ bootstrap/Bower.js
@@ -1,4 +1,4 @@
 "use strict";
 const NodeUtil = require("./Util");
 exports.Exec = NodeUtil.CreateNodeExec("bower", "bower --version", "bower/bin/bower");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQm93ZXIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJCb3dlci50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBWSxRQUFRLFdBQU0sUUFBUSxDQUFDLENBQUE7QUFFeEIsWUFBSSxHQUFHLFFBQVEsQ0FBQyxjQUFjLENBQ3ZDLE9BQU8sRUFDUCxpQkFBaUIsRUFDakIsaUJBQWlCLENBQ2xCLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQm93ZXIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJCb3dlci50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsbUNBQW1DO0FBRXhCLFFBQUEsSUFBSSxHQUFHLFFBQVEsQ0FBQyxjQUFjLENBQ3ZDLE9BQU8sRUFDUCxpQkFBaUIsRUFDakIsaUJBQWlCLENBQ2xCLENBQUMifQ==
