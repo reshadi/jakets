99262d3b0d61f2cbf519fa87bc23114afe6209f9
Mehrdad Reshadi <mreshadi@instartlogic.com>
2017-02-27 20:49:27 -0800

    had to pass the status code as error also down to call back. Since we cannot change the call back signature now, just converted the status code into a string error code

--- Jake.ts
+++ Jake.ts
@@ -39,7 +39,7 @@ export function Exec(cmd: string | string[], callback, isSilent?: boolean) {
     let executable = command[0];
     let args = command.slice(1);
     let result = ChildProcess.spawnSync(executable, args);
-    callback(result.error || "", (result.stdout || "").toString(), (result.stderr || "").toString());
+    callback(result.error || (result.status ? `exit code: ${result.status}` : ""), (result.stdout || "").toString(), (result.stderr || "").toString());
   } else {
     jake.exec(cmdArray, callback, { printStdout: true, printStderr: true });
   }
