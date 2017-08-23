73a9c63dfb32e45ffad5f74f789d273a05771039
Mehrdad Reshadi <mreshadi@instartlogic.com>
2017-08-22 20:58:25 -0700

    allow closure task to get a more structured set of options

--- Closure.ts
+++ Closure.ts
@@ -10,18 +10,51 @@ let ClosureJar = NodeUtil.FindModulePath("google-closure-compiler/compiler.jar",
 
 let RawExec = NodeUtil.CreateExec("java -jar " + ClosureJar);
 
-export function Exec(inputs: string, output: string, callback, options?: string, enableGzip?: boolean) {
-  let args = "";
-  //Default arguments that can be overwritten via options
-  args += " --compilation_level ADVANCED_OPTIMIZATIONS";
-  args += " --language_in ECMASCRIPT5";
-  // args += " --new_type_inf"; //Looks like crashes the compier sometimes
-  args += " --summary_detail_level 3";
-  // args += " --warning_level VERBOSE";
-  args += " --warning_level QUIET";
+export interface ClosureOptions {
+  //https://developers.google.com/closure/compiler/docs/api-ref
+  define?: string[];
+  summary_detail_level?: number;
+  // language?: "ECMASCRIPT3" | "ECMASCRIPT5" | "ECMASCRIPT5_STRICT" | "ECMASCRIPT6" | "ECMASCRIPT6_STRICT";
+  language?: "ES3" | "ES5" | "ECMASCRIPT5_STRICT" | "ECMASCRIPT6" | "ECMASCRIPT6_STRICT";
+  language_in?: ClosureOptions["language"],
+  language_out?: ClosureOptions["language"],
+  compilation_level?: "WHITESPACE_ONLY" | "SIMPLE_OPTIMIZATIONS" | "ADVANCED_OPTIMIZATIONS";
+  externs?: string[];
+  warning_level?: "QUIET" | "DEFAULT" | "VERBOSE";
+  output_wrapper?: string;
+  js_output_file?: string;
+}
 
-  args += " --js_output_file=" + output;
-  // args += " --jszip=" + output + ".gz";
+/** Default arguments that can be overwritten via options */
+export const DefaultClosureOptions: ClosureOptions = {
+  compilation_level: "ADVANCED_OPTIMIZATIONS",
+  // language: "ECMASCRIPT5",
+  language_in: "ES5",
+  //" --new_type_inf"; //Looks like crashes the compier sometimes
+  summary_detail_level: 3,
+  warning_level: "QUIET",
+};
+
+export function Exec(inputs: string, output: string, callback, options?: string, enableGzip?: boolean, closureOptions?: ClosureOptions) {
+  let allOptions = Object.assign({}, DefaultClosureOptions, { js_output_file: output }, closureOptions || {});
+
+  let args =
+    Object.keys(allOptions)
+      .map(option => {
+        let optionValue = allOptions[option];
+        let arg: string;
+
+        if (typeof optionValue === "string" || typeof optionValue === "number") {
+          arg = ` --${option} ${optionValue}`;
+        } else if (Array.isArray(optionValue)) {
+          arg = optionValue.map(v => ` --${option} ${v}`).join(" ");
+        } else {
+          throw `Does not know what to do with closure option ${option}:${optionValue}`;
+        }
+        return arg;
+      })
+      .join(" ");
+
   if (options) {
     args += " " + options;
   }
