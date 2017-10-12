0dc770f0f439d0abb884f2a14131681ff57ec602
Mehrdad Reshadi <mreshadi@instartlogic.com>
2017-10-12 14:43:44 -0700

    removed the test files to test what is causing the installation issue in npm with git+ protocl

--- Jakefile.ts
+++ Jakefile.ts
@@ -4,7 +4,7 @@ export * from "./lib/Jakets";
 import "./lib/Setup";
 import "./lib/GitUtil";
 
-import "./tests/Jakefile";
+//import "./tests/Jakefile";
 
 import { GlobalTask } from "./lib/Jakets";
 export const DefaultTask = GlobalTask("default").Description("Default task");
