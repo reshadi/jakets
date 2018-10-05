843457c92a58c51c5846bc8e03b36bcbf3645c8b
Mehrdad Reshadi <mreshadi@instartlogic.com>
2018-10-04 22:31:25 -0700

    release: 7.2.3 run the cli commands correctly on different os
    Testing: manual
    Tickets: NA

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const ChildProcess = require("child_process");
 require("jake");
+const Path = require("path");
+const Log_1 = require("../lib/Log");
 const Util = require("../lib/Util");
-const Exec = require("../lib/Exec");
 // jake.run.apply(jake, ["-t", "--jakefile", Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`), `jts:setup`]);
 var args = process.argv.slice(2);
 if (args.indexOf("jts:setup") === -1) {
-    Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
+    let command = Path.join("node_modules", ".bin", "jake") + ` -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`;
+    Log_1.Log(command, 0);
+    let setup = ChildProcess.execSync(command);
+    Log_1.Log(setup && setup.toString(), 0);
+    // Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
 }
 jake.run.apply(jake, args);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY2xpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLGdCQUFjO0FBQ2Qsb0NBQW9DO0FBQ3BDLG9DQUFvQztBQU1wQyx5SEFBeUg7QUFFekgsSUFBSSxJQUFJLEdBQUcsT0FBTyxDQUFDLElBQUksQ0FBQyxLQUFLLENBQUMsQ0FBQyxDQUFDLENBQUM7QUFDakMsSUFBSSxJQUFJLENBQUMsT0FBTyxDQUFDLFdBQVcsQ0FBQyxLQUFLLENBQUMsQ0FBQyxFQUFFO0lBQ3BDLElBQUksQ0FBQyxJQUFJLENBQUMsc0JBQXNCLElBQUksQ0FBQyx3QkFBd0IsQ0FBQyxHQUFHLFNBQVMsaUJBQWlCLENBQUMsWUFBWSxFQUFFLEdBQUcsRUFBRSxHQUFHLENBQUMsRUFBRSxJQUFJLENBQUMsQ0FBQztDQUM1SDtBQUNELElBQUksQ0FBQyxHQUFHLENBQUMsS0FBSyxDQUFDLElBQUksRUFBRSxJQUFJLENBQUMsQ0FBQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY2xpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLDhDQUE4QztBQUM5QyxnQkFBYztBQUNkLDZCQUE2QjtBQUM3QixvQ0FBaUM7QUFDakMsb0NBQW9DO0FBTXBDLHlIQUF5SDtBQUV6SCxJQUFJLElBQUksR0FBRyxPQUFPLENBQUMsSUFBSSxDQUFDLEtBQUssQ0FBQyxDQUFDLENBQUMsQ0FBQztBQUNqQyxJQUFJLElBQUksQ0FBQyxPQUFPLENBQUMsV0FBVyxDQUFDLEtBQUssQ0FBQyxDQUFDLEVBQUU7SUFDcEMsSUFBSSxPQUFPLEdBQUcsSUFBSSxDQUFDLElBQUksQ0FBQyxjQUFjLEVBQUUsTUFBTSxFQUFFLE1BQU0sQ0FBQyxHQUFHLGtCQUFrQixJQUFJLENBQUMsd0JBQXdCLENBQUMsR0FBRyxTQUFTLGlCQUFpQixDQUFDLFlBQVksQ0FBQztJQUNySixTQUFHLENBQUMsT0FBTyxFQUFFLENBQUMsQ0FBQyxDQUFDO0lBQ2hCLElBQUksS0FBSyxHQUFHLFlBQVksQ0FBQyxRQUFRLENBQUMsT0FBTyxDQUFDLENBQUM7SUFDM0MsU0FBRyxDQUFDLEtBQUssSUFBSSxLQUFLLENBQUMsUUFBUSxFQUFFLEVBQUUsQ0FBQyxDQUFDLENBQUM7SUFDbEMsOEhBQThIO0NBQy9IO0FBQ0QsSUFBSSxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsSUFBSSxFQUFFLElBQUksQ0FBQyxDQUFDIn0=
--- bin/cli.ts
+++ bin/cli.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+import * as ChildProcess from "child_process";
 import "jake";
+import * as Path from "path";
+import { Log } from "../lib/Log";
 import * as Util from "../lib/Util";
-import * as Exec from "../lib/Exec";
 
 //This is a copy of jake/bin/cli.js, only add the setup to it.
 declare namespace jake {
@@ -11,6 +13,10 @@ declare namespace jake {
 
 var args = process.argv.slice(2);
 if (args.indexOf("jts:setup") === -1) {
-  Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
+  let command = Path.join("node_modules", ".bin", "jake") + ` -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`;
+  Log(command, 0);
+  let setup = ChildProcess.execSync(command);
+  Log(setup && setup.toString(), 0);
+  // Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
 }
 jake.run.apply(jake, args);
