533de14786738294ac69623b51323a9aceb87727
Mehrdad Reshadi <mreshadi@instartlogic.com>
2018-10-04 20:56:25 -0700

    release: 7.2.2 minor fix of the cli to call setup correctly
    Testing: manual
    Tickets: NA

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("jake");
 const Util = require("../lib/Util");
-jake.run.apply(jake, ["-t", "--jakefile", Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`), `jts:setup`]);
+const Exec = require("../lib/Exec");
+// jake.run.apply(jake, ["-t", "--jakefile", Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`), `jts:setup`]);
 var args = process.argv.slice(2);
+if (args.indexOf("jts:setup") === -1) {
+    Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
+}
 jake.run.apply(jake, args);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY2xpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsZ0JBQWM7QUFDZCxvQ0FBb0M7QUFNcEMsSUFBSSxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsSUFBSSxFQUFFLENBQUMsSUFBSSxFQUFFLFlBQVksRUFBRSxJQUFJLENBQUMsd0JBQXdCLENBQUMsR0FBRyxTQUFTLGlCQUFpQixDQUFDLEVBQUUsV0FBVyxDQUFDLENBQUMsQ0FBQztBQUV0SCxJQUFJLElBQUksR0FBRyxPQUFPLENBQUMsSUFBSSxDQUFDLEtBQUssQ0FBQyxDQUFDLENBQUMsQ0FBQztBQUNqQyxJQUFJLENBQUMsR0FBRyxDQUFDLEtBQUssQ0FBQyxJQUFJLEVBQUUsSUFBSSxDQUFDLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY2xpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLGdCQUFjO0FBQ2Qsb0NBQW9DO0FBQ3BDLG9DQUFvQztBQU1wQyx5SEFBeUg7QUFFekgsSUFBSSxJQUFJLEdBQUcsT0FBTyxDQUFDLElBQUksQ0FBQyxLQUFLLENBQUMsQ0FBQyxDQUFDLENBQUM7QUFDakMsSUFBSSxJQUFJLENBQUMsT0FBTyxDQUFDLFdBQVcsQ0FBQyxLQUFLLENBQUMsQ0FBQyxFQUFFO0lBQ3BDLElBQUksQ0FBQyxJQUFJLENBQUMsc0JBQXNCLElBQUksQ0FBQyx3QkFBd0IsQ0FBQyxHQUFHLFNBQVMsaUJBQWlCLENBQUMsWUFBWSxFQUFFLEdBQUcsRUFBRSxHQUFHLENBQUMsRUFBRSxJQUFJLENBQUMsQ0FBQztDQUM1SDtBQUNELElBQUksQ0FBQyxHQUFHLENBQUMsS0FBSyxDQUFDLElBQUksRUFBRSxJQUFJLENBQUMsQ0FBQyJ9
--- bin/cli.ts
+++ bin/cli.ts
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 import "jake";
 import * as Util from "../lib/Util";
+import * as Exec from "../lib/Exec";
 
 //This is a copy of jake/bin/cli.js, only add the setup to it.
 declare namespace jake {
   function run(...args: any[]): void;
 }
-jake.run.apply(jake, ["-t", "--jakefile", Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`), `jts:setup`]);
+// jake.run.apply(jake, ["-t", "--jakefile", Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`), `jts:setup`]);
 
 var args = process.argv.slice(2);
+if (args.indexOf("jts:setup") === -1) {
+  Exec.Exec(`jake -t --jakefile ${Util.MakeRelativeToWorkingDir(`${__dirname}/../Jakefile.js`)} jts:setup`, () => { }, true);
+}
 jake.run.apply(jake, args);
